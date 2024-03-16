5b4663946634acf4968f0cd721141177f331c4cb
alexandre-Fotso <fyamgroups@gmail.com>
2024-03-16 10:31:05 +0100

    aout des modification du html

--- app.mjs
+++ app.mjs
@@ -5,7 +5,26 @@ import connectToDatabase from './helpers.mjs';
 const app = express();
 
 app.get("/", (req , res) => {
-    res.send('<h2>Hi Am alexandre fotso</h2>');
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>NodejsApp</title>
+        
+       
+    </head>
+    <body>
+    
+    
+        <h1>bonjour</h1>
+    
+        <script src="script.js"></script>
+        
+    </body>
+    </html>`);
 }
      
 );
